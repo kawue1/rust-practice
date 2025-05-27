0c3cffe453ce012cbd35acd4843c1383064681bd
Dmytro Demir <dmytro.dmytrenko@e-u.edu.ua>
2025-05-27 07:26:16 +0300

    виконав практичну 10

---
+++ src/homework/hw10.rs
@@ -0,0 +1,20 @@
+fn is_palindrome(x: u32) -> bool {
+    let s = x.to_string();
+    s.chars().eq(s.chars().rev())
+}
+
+fn main() {
+    let data = [
+        (123, false),
+        (121, true),
+        (1221, true),
+    ];
+
+    for (n, expected) in data.iter() {
+        let result = is_palindrome(*n);
+        println!("is_palindrome({}) = {}", n, result);
+        assert_eq!(result, *expected);
+    }
+
+    println!("All tests passed!");
+}
